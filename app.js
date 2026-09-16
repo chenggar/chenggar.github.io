8332bad20f39e8d0821d28843a9bcad97c36797e
Gary Cheng <chenggar7@gmail.com>
2026-09-16 13:10:43 -0400

    style: format event listener functions for consistency

--- app.js
+++ app.js
@@ -10,26 +10,26 @@ const close = document.querySelector(".close-icon");
 
 const videoList = [video1, video2, video3]
 
-videoList.forEach(function(video) {
-    video.addEventListener("mouseover", function(){
+videoList.forEach(function (video) {
+    video.addEventListener("mouseover", function () {
         video.play()
         hoverSign.classList.add("active")
     })
 
-    video.addEventListener("mouseout", function(){
+    video.addEventListener("mouseout", function () {
         video.pause()
         hoverSign.classList.remove("active")
     })
 })
 
 // Sidebar Elements
-menu.addEventListener("click", function(){
+menu.addEventListener("click", function () {
     sideBar.classList.remove("close-sidebar")
     sideBar.classList.add("open-sidebar")
     menu.classList.add("hide-menu")
 })
 
-close.addEventListener("click", function(){
+close.addEventListener("click", function () {
     sideBar.classList.remove("open-sidebar")
     sideBar.classList.add("close-sidebar")
     menu.classList.remove("hide-menu")
